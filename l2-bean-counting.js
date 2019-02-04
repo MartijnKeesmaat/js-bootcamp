862b40ce727bd47efb60ca0ae8526916c335a49c
Martijn Keesmaat <martijnpk@ziggo.nl>
2019-02-04 18:43:45 +0100

    Bean counting

--- l2-bean-counting.js
+++ l2-bean-counting.js
@@ -1,1 +1,24 @@
- 
+function countBs(str) {
+  let bCount = 0;
+  for (var i = 0; i < str.length; i++) {
+    if (str[i].match(/[bB]/)) {
+      bCount++
+    }
+  }
+  return bCount
+}
+
+console.log(countBs("BBC"));
+
+
+function countChar(str, char) {
+  let charCount = 0;
+  for (var i = 0; i < str.length; i++) {
+    if (str[i].toLowerCase() === char) {
+      charCount++
+    }
+  }
+  return charCount
+}
+
+console.log(countChar('BBC', 'c'));
