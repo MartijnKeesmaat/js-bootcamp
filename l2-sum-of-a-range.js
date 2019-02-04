1621793be926a9dce6ffc86f459c144c8b86f42d
Martijn Keesmaat <martijnpk@ziggo.nl>
2019-02-04 20:49:50 +0100

    Sum of range

--- l2-sum-of-a-range.js
+++ l2-sum-of-a-range.js
@@ -1,1 +1,16 @@
- 
+const range = (start, end) => {
+  let arr = [];
+  for (let i = 1; i <= end; i++) {
+    arr.push(i);
+  }
+  return arr
+}
+
+const sum = arr => {
+  return arr.reduce(reducer)
+}
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+
+console.log(sum(range(1, 10)));
