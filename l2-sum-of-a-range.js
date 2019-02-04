a74790d7df39b22c6f8f875d6139b64c801df516
Martijn Keesmaat <martijnpk@ziggo.nl>
2019-02-04 21:09:10 +0100

    ToDo look at bonus

--- l2-sum-of-a-range.js
+++ l2-sum-of-a-range.js
@@ -14,3 +14,14 @@ const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
 
 console.log(sum(range(1, 10)));
+
+
+
+// ToDo look at bonus exercise
+
+/*
+  As a bonus assignment, modify your range
+  function to take an optional third argument that indicates the“ step” value used when building the array.If no step is given, the elements go up by increments of one, corresponding to the old behavior.The
+  function call range(1, 10, 2) should
+  return [1, 3, 5, 7, 9].Make sure it also works with negative step values so that range(5, 2, -1) produces[5, 4, 3, 2].
+*/
